aaaf4a4b9dd93654acb3e0d416f594b1e973bdaa
Jacques Mock Schindler <jacques.mock@kbw.ch>
2024-07-24 19:29:13 +0200

    Überprüfe die statisitische Auswertung der Noten
    Es wird überprüft, ob die Noten Normalverteilt sind.
    Es wir eine Heatmap der Spearman Rangkorrelation erstellt.

--- docs/code/korrelation.py
+++ docs/code/korrelation.py
@@ -4,6 +4,10 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from matplotlib.colors import LinearSegmentedColormap
 import seaborn as sns
+from scipy.stats import shapiro
+from scipy.stats import kstest
+from scipy.stats import normaltest
+from scipy.stats import spearmanr
 
 # %%
 
@@ -25,6 +29,35 @@ indices = list(efz_reduced)
 
 # %%
 
+bm_verteilung = pd.DataFrame(index=headers, columns=['shapiro', 'kolmogorov', 'K2'])
+
+# %%
+
+resultat = normaltest(bm['D'])
+float(resultat.pvalue)
+
+
+# %%
+
+for header in headers:
+    resultat = normaltest(bm[header])
+    normal = 0.05 < float(resultat.pvalue)
+    bm_verteilung.loc[header, 'K2'] = normal
+    stat, p = shapiro(bm[header])
+    normal = 0.05 < float(p)
+    bm_verteilung.loc[header, 'shapiro'] = normal
+    stat, p = kstest(bm[header], 'norm')
+    normal = 0.05 < float(p)
+    bm_verteilung.loc[header, 'kolmogorov'] = normal
+        
+
+# %%
+
+bm_verteilung.to_csv("../data/normalverteilung_bm.csv", sep=";")
+    
+    
+# %%
+    
 correlations = pd.DataFrame(index=indices, columns=headers)
 
 # %%
@@ -68,5 +101,47 @@ plt.xlabel('BM Fächer')
 plt.savefig('../graphics/korrelationen_heatmap.svg',
             dpi=600)
 plt.show()
+
+# %%
+
+spearman = pd.DataFrame(index=indices, columns=headers)
+
+# %%
+
+for header in headers:
+    for index in indices:
+        tmp = pd.concat([bm[header], efz[index]], axis="columns")
+        tmp.dropna(axis="index", inplace=True)
+        tmpx = tmp[header]
+        tmpy = tmp[index]
+        cor, p = spearmanr(tmpx, tmpy)
+        spearman.loc[index, header] = cor
+        
+
+# %%
+
+spearman = spearman.select_dtypes(include=['object']).astype(float)
+
+# %%
+
+spearman.min()
+
+# %%
+
+plt.figure(
+    figsize = (7, 10)
+)
+ax = sns.heatmap(spearman, annot=True, 
+            cmap=grey_to_green, 
+            vmin=-.2, vmax=.6, center=0.3)
+for col in range(1, 9):
+    ax.axvline(x=col, color='black', linewidth=.5)
+plt.title(f'Spearmankorrelation Noten BM - EFZ ')
+plt.ylabel('EFZ Modul Nr.')
+plt.xlabel('BM Fächer')
+plt.savefig('../graphics/spearmankorrelationen_heatmap.svg',
+            dpi=600)
+plt.show()
+
 
 # %%
