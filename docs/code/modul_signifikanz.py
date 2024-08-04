6a48c8d98b5858c059c152083972e2267992e26b
Jacques Mock Schindler <jacques.mock@kbw.ch>
2024-08-04 11:22:56 +0200

    Automatisiere MoE Berechnung

--- docs/code/modul_signifikanz.py
+++ docs/code/modul_signifikanz.py
@@ -1,7 +1,22 @@
 # %%
 
 import pandas as pd
+import numpy as np
 from math import sqrt
+
+# %%
+
+df = pd.read_csv('../data/noten_efz.csv',
+                 sep=';',
+                 index_col=0)
+
+# %%
+
+numbers = df.count()
+numbers = numbers.where(numbers >= 20, np.nan)
+numbers = pd.Series(numbers.dropna())
+numbers.sort_values(inplace=True)
+numbers = numbers.unique().tolist()
 
 # %%
 
@@ -10,6 +25,12 @@ def margin_error(s, n, p=0.5, z=1.96):
 
 # %%
 
-margin_error(167, 400)
+moe = []
+
+for n in numbers:
+    e = margin_error(n, 400)
+    moe.append((n, e))
+
+print(moe)    
 
 # %%
