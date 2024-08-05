1e3c49938569c1a943215b5f106eecd3c38167c9
Jacques Mock Schindler <jacques.mock@kbw.ch>
2024-08-05 19:03:55 +0200

    Räume Gruppierungsskript auf

--- docs/code/korrelationen_gruppiert.py
+++ docs/code/korrelationen_gruppiert.py
@@ -36,44 +36,6 @@ gw = gw.reindex(gw_indices)
 
 # %%
 
-colors = ['#808080', '#A0A0A0', '#C0C0C0', '#E0E0E0',  # Greys
-          '#F0F0F0',  # Very light grey (almost white) for the center
-          '#E0FFE0', '#C0FFC0', '#80FF80', '#40FF40', '#00FF00']  # Greens
-n_bins = 256  # Number of color gradations
-grey_to_green = LinearSegmentedColormap.from_list('custom_grey_green', colors, N=n_bins)
-
-# %%
-
-plt.figure(
-    figsize = (7, 10)
-)
-ax = sns.heatmap(gw, annot=True, 
-            cmap=grey_to_green, 
-            vmin=-.2, vmax=.6, center=0.3)
-for col in range(1, 9):
-    ax.axvline(x=col, color='black', linewidth=.5)
-    
-yticklabels = ax.get_yticklabels()
-for label in yticklabels:
-    if label.get_text() in ['100', '118', '122',
-                            '153', '226', '226a',
-                            '226b', '242', '306',
-                            '326', '335', '403',
-                            '411', '431']:
-        label.set_color('red')
-    else:
-        label.set_color('black')
-
-plt.title(f'Spearmankorrelation Noten Geisteswissenschaften - EFZ ')
-plt.ylabel('EFZ Modul Nr.')
-plt.xlabel('Geisteswissenschaften')
-plt.savefig('../graphics/spearmankorrelationen_heatmap.png',
-            format='png',
-            dpi=300)
-plt.show()
-
-# %%
-
 sw = pd.DataFrame([df['WR'], df['TU']]).T
 
 # %%
@@ -87,35 +49,6 @@ sw_header = sw_header.index.to_list()
 
 sw = sw[sw_header]
 sw = sw.reindex(sw_indices)
-
-# %%
-
-plt.figure(
-    figsize = (7, 10)
-)
-ax = sns.heatmap(sw, annot=True, 
-            cmap=grey_to_green, 
-            vmin=-.2, vmax=.6, center=0.3)
-for col in range(1, 9):
-    ax.axvline(x=col, color='black', linewidth=.5)
-    
-yticklabels = ax.get_yticklabels()
-for label in yticklabels:
-    if label.get_text() in ['100', '118', '122',
-                            '153', '226', '226a',
-                            '226b', '242', '306',
-                            '326', '335', '403',
-                            '411', '431']:
-        label.set_color('red')
-    else:
-        label.set_color('black')
-
-plt.title(f'Spearmankorrelation Noten Sozialwissenschaften - EFZ ')
-plt.ylabel('EFZ Modul Nr.')
-plt.xlabel('Sozialwissenschaften')
-# plt.savefig('../graphics/spearmankorrelationen_heatmap.svg',
-#            dpi=600)
-plt.show()
 
 # %%
 
@@ -135,44 +68,9 @@ aw = aw.reindex(aw_indices)
 
 # %%
 
-plt.figure(
-    figsize = (7, 10)
-)
-ax = sns.heatmap(aw, annot=True, 
-            cmap=grey_to_green, 
-            vmin=-.2, vmax=.6, center=0.3)
-for col in range(1, 9):
-    ax.axvline(x=col, color='black', linewidth=.5)
-    
-yticklabels = ax.get_yticklabels()
-for label in yticklabels:
-    if label.get_text() in ['100', '118', '122',
-                            '153', '226', '226a',
-                            '226b', '242', '306',
-                            '326', '335', '403',
-                            '411', '431']:
-        label.set_color('red')
-    else:
-        label.set_color('black')
-
-plt.title(f'Spearmankorrelation Noten abstrakte Wissenschaften - EFZ ')
-plt.ylabel('EFZ Modul Nr.')
-plt.xlabel('abstrakte Wissenschaften')
-# plt.savefig('../graphics/spearmankorrelationen_heatmap.svg',
-#            dpi=600)
-plt.show()
-
-# %%
-
 modulreihenfolge_gw = gw.index.to_list()
 modulreihenfolge_sw = sw.index.to_list()
 modulreihenfolge_aw = aw.index.to_list()
-
-# %%
-
-print(modulreihenfolge_gw)
-print(modulreihenfolge_sw)
-print(modulreihenfolge_aw)
 
 # %%
 
@@ -230,6 +128,3 @@ plt.savefig('../graphics/verschiebung.png',
 plt.show()
 # %%
 
-module.dtypes
-
-# %%
