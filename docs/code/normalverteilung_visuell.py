80061e2aa3f65dcf726190dd1542ef264afe5d03
Jacques Mock Schindler <jacques.mock@kbw.ch>
2024-08-06 09:09:46 +0200

    Ergänze Plot für EFZ

--- docs/code/normalverteilung_visuell.py
+++ docs/code/normalverteilung_visuell.py
@@ -4,13 +4,24 @@ import pandas as pd
 import numpy as np
 from scipy.stats import norm
 import matplotlib.pyplot as plt
-
+import math
 
 # %%
 
-df = pd.read_csv('../data/noten_bm.csv',
+bms = pd.read_csv('../data/noten_bm.csv',
                  sep=';',
                  index_col=0)
+
+efz = pd.read_csv('../data/noten_efz.csv',
+                  sep=';',
+                  index_col=0)
+
+# %%
+
+threshod = 20
+columns_counts = efz.count()
+columns_to_drop = columns_counts[columns_counts < threshod].index.to_list()
+efz.drop(columns=columns_to_drop, inplace=True)
 
 # %%
 
@@ -24,36 +35,155 @@ def create_histogramms(dataframe):
         plt.figure()
         
         # Histogramm plotten
-        n, bins, patches = plt.hist(dataframe[column],
+        n, bins, patches = plt.hist(dataframe[column].dropna(),
                                     bins=bins, alpha=0.7,
                                     edgecolor='black', 
                                     density=True)
         
         # Normalverteilungskurve berechnen
-        mu, std = norm.fit(dataframe[column])
+        mu, std = norm.fit(dataframe[column].dropna())
         xmin, xmax = plt.xlim()
         x = np.linspace(xmin, xmax, 100)
         p = norm.pdf(x, mu, std)
         
         # Normalverteilungskurve plotten
-        plt.plot(x, p, 'k', linewidth=2)
-        title = f'Histogramm und Normalverteilung für {column}\n$\mu={mu:.2f}$, $\sigma={std:.2f}$'
+        plt.plot(x, p, 'k', 
+                 linewidth=1, linestyle='--',
+                 label='Normalverteilung')
+        plt.title(f'Histogramm und Normalverteilung für {column}\n'
+                  + r' $\mu=$' +f'{mu:.2f}' 
+                  + r' $\sigma=$' +f'{std:.2f}')
         
         # Beschriftungen und Darstellung
-        plt.title(title)
         plt.xlabel('Noten')
         plt.ylabel('Häufigkeit')
         plt.xticks(bin_centers)
         plt.grid(axis='y')
+        plt.legend()
         
         plt.show()
         
 
 # %%
 
-df.drop('IDAF', axis='columns', inplace=True)
+def create_histogramms_grid(dataframe):
+    # Vorbereitung für die Einteilung in halbe Noten und Aufbereitung
+    # der Datenspalten
+    bins = [0.75, 1.25, 1.75, 2.25, 2.75, 3.25, 3.75, 4.25, 4.75, 5.25, 5.75, 6.25]
+    bin_centers = [1, 1.5, 2, 2.5, 3, 3.5, 4, 4.5, 5, 5.5, 6]
+    
+    # Erstelle eine 3x3 Figur
+    fig, axs = plt.subplots(3, 3, figsize=(15, 15))
+    fig.suptitle('Histogramme und Normalverteilungen der Noten der BMS', fontsize=16)
+    
+    for idx, column in enumerate(dataframe.columns):
+        row = idx // 3
+        col = idx % 3
+        ax = axs[row, col]
+        
+        # Histogramm plotten
+        n, bins, patches = ax.hist(dataframe[column].dropna(),
+                                   bins=bins, alpha=0.7,
+                                   edgecolor='black', 
+                                   density=True)
+        
+        # Normalverteilungskurve berechnen
+        mu, std = norm.fit(dataframe[column].dropna())
+        xmin, xmax = ax.get_xlim()
+        x = np.linspace(xmin, xmax, 100)
+        p = norm.pdf(x, mu, std)
+        
+        # Normalverteilungskurve plotten
+        ax.plot(x, p, 'k', 
+                linewidth=1, linestyle='--',
+                label='Normalverteilung')
+        ax.set_title(f'{column}\n' 
+                     + r' $\mu=$' +f'{mu:.2f}' 
+                     + r' $\sigma=$' +f'{std:.2f}')
+        
+        # Beschriftungen und Darstellung
+        ax.set_xlabel('Noten')
+        ax.set_ylabel('Häufigkeit')
+        ax.set_xticks(bin_centers)
+        ax.grid(axis='y')
+        ax.legend()
+    
+    plt.tight_layout()
+    plt.savefig('../graphics/normalverteilung_bm.png',
+                dpi=300)
+    plt.show()
 
 # %%
 
-create_histogramms(df)
+create_histogramms_grid(bms)
+# %%
+
+def create_histogramms_grid_efz(dataframe):
+    # Vorbereitung für die Einteilung in halbe Noten und Aufbereitung
+    # der Datenspalten
+    bins = [0.75, 1.25, 1.75, 2.25, 2.75, 3.25, 3.75, 4.25, 4.75, 5.25, 5.75, 6.25]
+    bin_centers = [1, 1.5, 2, 2.5, 3, 3.5, 4, 4.5, 5, 5.5, 6]
+    
+    # Berechne die Anzahl der Zeilen basierend auf der Anzahl der Spalten
+    num_cols = len(dataframe.columns)
+    num_rows = math.ceil(num_cols / 3)
+    
+    # Erstelle eine Figur mit der berechneten Anzahl von Zeilen und 3 Spalten
+    fig, axs = plt.subplots(num_rows, 3, figsize=(15, 5*num_rows + 1))
+    
+    fig.suptitle('Histogramme und Normalverteilungen', fontsize=16, y=1.005)
+    
+    for idx, column in enumerate(dataframe.columns):
+        row = idx // 3
+        col = idx % 3
+        
+        # Überprüfe, ob axs ein 2D-Array ist (mehr als eine Zeile)
+        if num_rows > 1:
+            ax = axs[row, col]
+        else:
+            ax = axs[col]
+        
+        # Histogramm plotten
+        n, bins, patches = ax.hist(dataframe[column].dropna(),
+                                   bins=bins, alpha=0.7,
+                                   edgecolor='black', 
+                                   density=True)
+        
+        # Normalverteilungskurve berechnen
+        mu, std = norm.fit(dataframe[column].dropna())
+        xmin, xmax = ax.get_xlim()
+        x = np.linspace(xmin, xmax, 100)
+        p = norm.pdf(x, mu, std)
+        
+        # Normalverteilungskurve plotten
+        ax.plot(x, p, 'k', 
+                linewidth=1, linestyle='--',
+                label='Normalverteilung')
+        ax.set_title(f'Modul {column}\n' 
+                     + r' $\mu=$' +f'{mu:.2f}' 
+                     + r' $\sigma=$' +f'{std:.2f}')
+        
+        # Beschriftungen und Darstellung
+        ax.set_xlabel('Noten')
+        ax.set_ylabel('Häufigkeit')
+        ax.set_xticks(bin_centers)
+        ax.grid(axis='y')
+        ax.legend()
+    
+    # Entferne leere Subplots
+    for idx in range(num_cols, num_rows * 3):
+        row = idx // 3
+        col = idx % 3
+        if num_rows > 1:
+            fig.delaxes(axs[row, col])
+        else:
+            fig.delaxes(axs[col])
+    
+    plt.tight_layout()
+    plt.savefig('../graphics/normalverteilung_efz.png',
+                dpi=300)
+    plt.show()
+# %%
+
+create_histogramms_grid_efz(efz)
 # %%
